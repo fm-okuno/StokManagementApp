1139df2e074d03a2917189b649f973c221f9836c
fm-okuno <okuno@freemake.co.jp>
2020-04-23 14:24:52 +0900

    プロジェクトを作成
    プロジェクト名「StokManagementApp」

---
+++ StokManagementApp/StokManagementApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StokManagementApp
+//
+//  Created by 開発アカウント on 2020/04/23.
+//  Copyright © 2020 開発アカウント. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
