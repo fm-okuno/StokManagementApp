eeb02c50004b742e7aac245566b80dce0d8af992
fm-okuno <okuno@freemake.co.jp>
2020-04-23 17:36:29 +0900

    数値の入力欄を追加し、3桁毎にカンマで区切るようにした
    Main.storyboard
    　・LabelとStepperを配置、Autolayoutの設定、ViewController.swiftと接続
    　　→Label名「amountLabel」
    	→Stepper名「actionChangeAmountButton」
    　・actionChangeAmountButtonには0~9,999までの値しか保持しないように設定
    
    ViewController.swift
    　・actionChangeAmountButtonの保持する値を3桁毎のカンマで区切り、amountLabelに表示する処理を実装

--- StokManagementApp/StokManagementApp/ViewController.swift
+++ StokManagementApp/StokManagementApp/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var amountLabel: UILabel!
+    var intAmount: Int = 0
+    
+    @IBAction private func actionChangeAmountButton(_ sender: UIStepper) {
+        //UIStepperの値をDoubleからIntに変換し、intAmountに代入
+        intAmount = Int(sender.value)
+        
+        //3桁毎にカンマで区切る設定
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.groupingSeparator = ","
+        formatter.groupingSize = 3
+        
+        //intAmountをNSNumber型に変換
+        let nsAmount = NSNumber(value: intAmount)
+        //変換したnsAmountに3桁区切りの設定を適用
+        let addCommaAmount = formatter.string(from: nsAmount)
+        
+        //カンマで区切ったStepperの値をamountLabel.textに代入して表示
+        amountLabel.text = addCommaAmount
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
