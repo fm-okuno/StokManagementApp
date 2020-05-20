564e9aab1f4ad65da1e26f0119223976c9fdfcbc
fm-okuno <okuno@freemake.co.jp>
2020-05-20 18:07:34 +0900

    選択ずみの画像は一覧/入力画面でリストの行ごとに管理する、選択済みの画像があれば表示する
    DetailViewController.swift
    ・カメラロールで選択した写真をImageViewに表示する処理を実装
    ・userDefaultsで写真を本体に保存できるよう実装
    ・Saveボタンを押下で表示している写真を保存
    ・写真が保管されている行の詳細ボタンが押下されたら、その行に結びつけて保存してある写真を表示
    
    ViewController.swift
    ・セグエで詳細ボタンを押下した行のIndexPathを受け渡し
    
    Main.storyboard
    ・AutoLayoutを設定し、端末を変えても表示が崩れないようにした

--- StokManagementApp/StokManagementApp/DetailViewController.swift
+++ StokManagementApp/StokManagementApp/DetailViewController.swift
@@ -16,8 +16,12 @@ class DetailViewController : UIViewController {
     @IBOutlet private weak var imageView: UIImageView!
     
     //MARK: - instance
-    //titleTextにViewControllerから受け取ったamountArrayのテキストを表示
+    //titleTextに受け取ったamountArrayのテキストを表示
     var titleText: String?
+    //詳細が押下されたセルのIndexPath
+    var imageRow: Int?
+    //写真を保存しておく為のuserDefaults
+    let userDefaults = UserDefaults.standard
 
     //MARK: - public method
     override func viewDidLoad() {
@@ -31,11 +35,61 @@ class DetailViewController : UIViewController {
         
         //titleにtextの内容を代入して表示
         uiNavigationItem.title = thisTitleText
+        
+        //imageRowに値が入っていない場合には、後続処理を継続しない
+        guard let thisImageRow = imageRow else {
+            return
+        }
+        
+        //行番号thisImageRowをKeyとして使用するためにStringに変換
+        let stringImageRow = convertString(thisImageRow)
 
+        //詳細ボタンが押下されたセルに写真が保存されていた場合にはimageViewに保存してある写真を表示
+        if userDefaults.object(forKey: stringImageRow) != nil {
+            let canNotShowImage = userDefaults.data(forKey: stringImageRow)
+            
+            //rowImageDataが空の場合には、後続処理を継続しない
+            guard let thisCanNotShowImage = canNotShowImage else {
+                return
+            }
+            
+            //Data型をUIImage型に変換
+            let canShowImage = UIImage(data: thisCanNotShowImage)
+            imageView.image = canShowImage
+        }
+    }
+    
+    //MARK: - private method
+    
+    private func convertString(_ to: Int) -> String {
+        return String(to)
     }
     
     //MARK: - IBAction
     
+    //写真を保存する為のactionSaveImageButton
+    @IBAction private func actionSaveImageButton(_ sender: Any) {
+        //現在表示中の画像データを取得
+        let image = imageView.image
+        
+        //imageが空の場合には後続処理を継続しない
+        guard let thisImage = image else {
+            return
+        }
+        
+        let imageData = thisImage.pngData()
+        
+        //imageRowが空の場合には後続処理を継続しない
+        guard let thisImageRow = imageRow else {
+            return
+        }
+        
+        let stringImageRow = convertString(thisImageRow)
+        
+        //Keyに受け取ったRow、Valueに写真を指定して本体に収納
+        userDefaults.setValue(imageData, forKey: stringImageRow)
+        userDefaults.synchronize()
+    }
     //+ボタンを押下でカメラロールを開き、写真を選択
     @IBAction private func actionSelectImageButton(_ sender: Any) {
             //写真を選ぶビュー
@@ -57,8 +111,10 @@ extension DetailViewController: UIImagePickerControllerDelegate, UINavigationCon
         
         //選択された写真を取得
         let image = info[.originalImage] as! UIImage
-        //ビューに表示
+
+        //選択された写真を表示
         imageView.image = image
+        
         //写真を選ぶビューを閉じる
         self.dismiss(animated: true )
     }
