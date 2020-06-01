7010e1ca38a4f6585f0c33c7df6ab47b96f18d7a
fm-okuno <okuno@freemake.co.jp>
2020-06-01 18:24:02 +0900

    privateとして実装できるメソッドだったので、//MARK: - private method内へ移動

--- StokManagementApp/StokManagementApp/ViewController.swift
+++ StokManagementApp/StokManagementApp/ViewController.swift
@@ -56,7 +56,6 @@ class ViewController: UIViewController {
         )
     }
     
-    
     //segueで遷移時の処理
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
@@ -70,20 +69,6 @@ class ViewController: UIViewController {
         
         //detailVCのtitleTextにamountArrayを入れた変数を指定
         detailVC.titleText = sendText
-    }
-    
-    //DBにデータを追加する為のaddStockDataメソッド
-    func addStockData(amount: Int, comment: String?, amountImage: Data?, createDate: String) -> StockModel {
-        let addStockData = StockModel.create(realm: realm)
-        addStockData.amount = amount
-        addStockData.comment = comment
-        addStockData.amountImage = amountImage
-        addStockData.createDate = createDate
-        try! realm.write {
-            realm.add(addStockData)
-        }
-        //printで確認するためにaddStockDataを返す
-        return addStockData
     }
     
     //MARK: - IBAction
@@ -132,6 +117,7 @@ class ViewController: UIViewController {
         //追加ボタン押下で選択が全解除される為、一度sumAmountを初期化
         sumAmount = 0
         
+        //テーブルを更新
         tableView.reloadData()
     }
     
@@ -161,6 +147,20 @@ class ViewController: UIViewController {
     }
     
     //MARK: - private method
+    
+    //DBにデータを追加する為のaddStockDataメソッド
+    private func addStockData(amount: Int, comment: String?, amountImage: Data?, createDate: String) -> StockModel {
+        let addStockData = StockModel.create(realm: realm)
+        addStockData.amount = amount
+        addStockData.comment = comment
+        addStockData.amountImage = amountImage
+        addStockData.createDate = createDate
+        try! realm.write {
+            realm.add(addStockData)
+        }
+        //printで確認するためにaddStockDataを返す
+        return addStockData
+    }
     
     //Int型の値を受け取り、3桁毎にカンマで区切る「addComma」メソッド
     private func convertAmountToString(_ forValue: Int) -> String? {
